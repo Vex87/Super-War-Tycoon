0f73dc4baf90c94c98d1048c1394727293401158
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-08-26 18:15:20 -0400

    Multiple tower support & multiplier reward
    - Each tower increases the amount of money you make by a multiplier

--- Conquest-Point/src/ServerScriptService/ConquestPoint.server.lua
+++ Conquest-Point/src/ServerScriptService/ConquestPoint.server.lua
@@ -1,75 +1,92 @@
 -- // Settings \\ --
 
 local UPDATE_DEPLAY = 1
-local CAPTURE_REWARD = 1
+local MONEY_MULTIPLIER = 1
 local MAX_PARTS = math.huge
 local NEUTRAL_COLOR = BrickColor.new("Institutional white")
+local POINT_TAG = "Point"
 
 -- // Variables \\ --
 
-local ConquestPoint = workspace.ConquestPoint
-local Beam = ConquestPoint.Beam
-local Point1 = ConquestPoint.Point1
-local Point2 = ConquestPoint.Point2
-
--- Creates the region
-local Region = Region3.new(Point1.Position, Point2.Position)
-local PlayersInRegion = {}
-local CurrentOwner
-
-local IgnoreList = {ConquestPoint}
+local CollectionService = game:GetService("CollectionService")
+local Core = require(game.ReplicatedStorage.Core)
+local Points = {}
 
 -- // Functions \\ --
 
-function RemovePlayers(PartsInRegion)
-    -- If the root in PlayersInRegion is not in PartsInRegion or the player is dead, the player will be removed
-    for i,Root in pairs(PlayersInRegion) do
-        if Root.Parent.Humanoid.Health == 0 or not table.find(PartsInRegion, Root) then
-            table.remove(PlayersInRegion, i)
+function RemovePlayers(Info)
+    for i,p in pairs(Info.PlayersInRegion) do
+        local Char = p.Character
+        if Char.Humanoid.Health == 0 or not table.find(Info.PartsInRegion, Char.HumanoidRootPart) then
+            table.remove(Info.PlayersInRegion, i)
         end
     end
 end
 
-function AddPlayers(PartsInRegion)
-    -- If the part in the region is a player, it isn't in PlayersInRegion, and it is alive; it will be added to PlayersInRegion
-    for _,Part in pairs(PartsInRegion or {}) do
+function AddPlayers(Info)
+    for _,Part in pairs(Info.PartsInRegion or {}) do
         local Char = Part.Parent
-        if game.Players:FindFirstChild(Char.Name) and Char:FindFirstChild("HumanoidRootPart") and not table.find(PlayersInRegion, Char.HumanoidRootPart) and Char.Humanoid.Health > 0 then
-            table.insert(PlayersInRegion, Char.HumanoidRootPart)
+        local p = game.Players:FindFirstChild(Char.Name)
+        if p and Char:FindFirstChild("HumanoidRootPart") and not table.find(Info.PlayersInRegion, p) and Char.Humanoid.Health > 0 then
+            table.insert(Info.PlayersInRegion, p)
         end
     end
 end
 
-function AddPoints()
-    -- Sets current owner to the only person in the region if applicable, gives them money
+function AddPoints(Info)
+    if #Info.PlayersInRegion == 1 and Info.CurrentOwner ~= Info.PlayersInRegion[1] then
+        
+        if Info.CurrentOwner then
+            Info.CurrentOwner.Stats.MoneyMultiplier.Value = Info.CurrentOwner.Stats.MoneyMultiplier.Value - MONEY_MULTIPLIER
+        end
 
-    if #PlayersInRegion == 1 then
-        local Root = PlayersInRegion[1]
-        CurrentOwner = Root
-    elseif #PlayersInRegion > 1 then
-        CurrentOwner = nil
-    end
+        Info.CurrentOwner = Info.PlayersInRegion[1]
+        Info.CurrentOwner.Stats.MoneyMultiplier.Value = Info.CurrentOwner.Stats.MoneyMultiplier.Value + MONEY_MULTIPLIER
 
-    if CurrentOwner then
-        game.Players[CurrentOwner.Parent.Name].leaderstats.Money.Value = game.Players[CurrentOwner.Parent.Name].leaderstats.Money.Value + CAPTURE_REWARD
+    elseif #Info.PlayersInRegion > 1 then
+        if Info.CurrentOwner then
+            Info.CurrentOwner.Stats.MoneyMultiplier.Value = Info.CurrentOwner.Stats.MoneyMultiplier.Value - MONEY_MULTIPLIER
+        end
+        Info.CurrentOwner = nil
     end
 end
 
-function Visualize()
-    -- If there is a CurrentOwner, then the beam will be the owner's team TeamColor or a neutral color if not applicable
-    if CurrentOwner then
-        Beam.BrickColor = game.Players[CurrentOwner.Parent.Name].TeamColor
+function Visualize(Info)
+    if Info.CurrentOwner then
+        Info.Point.Beam.BrickColor = Info.CurrentOwner.TeamColor
     else
-        Beam.BrickColor = NEUTRAL_COLOR
+        Info.Point.Beam.BrickColor = NEUTRAL_COLOR
     end
+end
+
+function Initiate(Point)
+
+    local Info = {
+        Point = Point,
+        Region = Region3.new(Point.Point1.Position, Point.Point2.Position),
+        PlayersInRegion = {},
+        CurrentOwner = nil,    
+        IgnoreList = {Point},
+        PartsInRegion = {}
+    }
+    Points[Point.Name] = Info
+
+    while wait(UPDATE_DEPLAY) do
+        Info.PartsInRegion = workspace:FindPartsInRegion3WithIgnoreList(Info.Region, Info.IgnoreList, MAX_PARTS)
+        RemovePlayers(Info)
+        AddPlayers(Info)
+        AddPoints(Info)
+        Visualize(Info)
+    end
+
 end
 
 -- // Main \\ --
 
-while wait(UPDATE_DEPLAY) do
-    local PartsInRegion = workspace:FindPartsInRegion3WithIgnoreList(Region, IgnoreList, MAX_PARTS)
-    RemovePlayers(PartsInRegion)
-    AddPlayers(PartsInRegion)
-    AddPoints()
-    Visualize()
+for _,Point in pairs(CollectionService:GetTagged(POINT_TAG)) do
+    Core.NewThread(Initiate, Point)
 end
+
+CollectionService:GetInstanceAddedSignal(POINT_TAG):Connect(function(Point)
+    Core.NewThread(Initiate, Point)
+end)
--- Conquest-Point/src/ServerScriptService/leaderstats.server.lua
+++ Conquest-Point/src/ServerScriptService/leaderstats.server.lua
@@ -1,9 +1,41 @@
+local REWARD_DELAY = 1
+
+workspace.IncreaseRevenue.ClickDetector.MouseClick:Connect(function(p)
+    p.Stats.Revenue.Value = p.Stats.Revenue.Value + 1
+end)
+
+workspace.DecreaseRevenue.ClickDetector.MouseClick:Connect(function(p)
+    p.Stats.Revenue.Value = p.Stats.Revenue.Value - 1
+end)
+
 game.Players.PlayerAdded:Connect(function(p)
+
+    -- Leaderstats
     local leaderstats = Instance.new("Folder")
     leaderstats.Name = "leaderstats"
     leaderstats.Parent = p
-    
+
     local Money = Instance.new("IntValue")
     Money.Name = "Money"
     Money.Parent = leaderstats   
+
+    -- Stats
+    local Stats = Instance.new("Folder")
+    Stats.Name = "Stats"
+    Stats.Parent = p
+
+    local Revenue = Instance.new("IntValue")
+    Revenue.Name = "Revenue"
+    Revenue.Value = 1
+    Revenue.Parent = Stats 
+
+    local MoneyMultiplier = Instance.new("NumberValue")
+    MoneyMultiplier.Name = "MoneyMultiplier"
+    MoneyMultiplier.Value = 1
+    MoneyMultiplier.Parent = Stats 
+
+    while wait(REWARD_DELAY) do
+        Money.Value = Money.Value + (Revenue.Value * MoneyMultiplier.Value)
+    end
+
 end)
