9c76b1e8e4c4a3c33473c54a9ef4c6db4e392d98
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-08-31 17:09:30 -0400

    Added different crate types & weapon rewards

--- Main/src/ServerScriptService/Crates/Rewards.lua
+++ Main/src/ServerScriptService/Crates/Rewards.lua
@@ -1,39 +1,198 @@
 local Rewards = {
+    Crates = {},
     Objects = {},
     Functions = {},
-    AllRewards = {},
+    Rewards = {},
+}
+local CrateStorage = game.ServerStorage.Objects.Crates
+local WeaponStorage = game.ServerStorage.Objects.Weapons
+
+Rewards.Crates = {
+    Air = CrateStorage.Air;
+    Doom = CrateStorage.Doom;
+    Fire = CrateStorage.Fire;
+    God = CrateStorage.God;
+    Water = CrateStorage.Water;
 }
 
 Rewards.Objects = {
     Money100 = {
         Type = "Money",
-        Chance = 5,
+        Chance = 31,
+        Crates = {"Air", "Doom", "Fire", "God", "Water"},
         Amount = 100
     },
     Money200 = {
         Type = "Money",
-        Chance = 4,
+        Chance = 30,
+        Crates = {"Air", "Doom", "Fire", "God", "Water"},
         Amount = 200
     },
     Money300 = {
         Type = "Money",
-        Chance = 3,
+        Chance = 29,
+        Crates = {"Air", "Doom", "Fire", "God", "Water"},
         Amount = 300
     },
     Money400 = {
         Type = "Money",
-        Chance = 2,
+        Chance = 27,
+        Crates = {"Air", "Doom", "Fire", "God", "Water"},
         Amount = 400
     },
     Money500 = {
         Type = "Money",
-        Chance = 1,
+        Chance = 25,
+        Crates = {"Air", "Doom", "Fire", "God", "Water"},
         Amount = 500
+    },
+    ["Air Auto Pistol"] = {
+        Type = "Weapon",
+        Chance = 15,
+        Crates = {"Air"}
+    },
+    ["Air Shotgun"] = {
+        Type = "Weapon",
+        Chance = 11,
+        Crates = {"Air"}
+    },
+    ["Air Knife"] = {
+        Type = "Weapon",
+        Chance = 22,
+        Crates = {"Air"}
+    },
+    ["Air Pistol"] = {
+        Type = "Weapon",
+        Chance = 19,
+        Crates = {"Air"}
+    },
+    ["Air Rifle"] = {
+        Type = "Weapon",
+        Chance = 6,
+        Crates = {"Air"}
+    },
+    ["Air Rocket Launcher"] = {
+        Type = "Weapon",
+        Chance = 1,
+        Crates = {"Air"}
+    },
+    ["Doom Auto Pistol"] = {
+        Type = "Weapon",
+        Chance = 15,
+        Crates = {"Doom"}
+    },
+    ["Doom Shotgun"] = {
+        Type = "Weapon",
+        Chance = 11,
+        Crates = {"Doom"}
+    },
+    ["Doom Knife"] = {
+        Type = "Weapon",
+        Chance = 22,
+        Crates = {"Doom"}
+    },
+    ["Doom Pistol"] = {
+        Type = "Weapon",
+        Chance = 19,
+        Crates = {"Doom"}
+    },
+    ["Doom Rifle"] = {
+        Type = "Weapon",
+        Chance = 6,
+        Crates = {"Doom"}
+    },
+    ["Doom Rocket Launcher"] = {
+        Type = "Weapon",
+        Chance = 1,
+        Crates = {"Doom"}
+    },
+    ["Electric Auto Pistol"] = {
+        Type = "Weapon",
+        Chance = 15,
+        Crates = {"God"},
+    },
+    ["Electric Knife"] = {
+        Type = "Weapon",
+        Chance = 22,
+        Crates = {"God"},
+    },
+    ["Electric Pistol"] = {
+        Type = "Weapon",
+        Chance = 19,
+        Crates = {"God"},
+    },
+    ["Electric Rocket Launcher"] = {
+        Type = "Weapon",
+        Chance = 1,
+        Crates = {"God"},
+    },
+    ["Electric Shotgun"] = {
+        Type = "Weapon",
+        Chance = 20,
+        Crates = {"God"},
+    },
+    ["Fire Auto Pistol"] = {
+        Type = "Weapon",
+        Chance = 11,
+        Crates = {"Fire"},
+    },
+    ["Fire Pistol"] = {
+        Type = "Weapon",
+        Chance = 19,
+        Crates = {"Fire"},
+    },
+    ["Fire Rifle"] = {
+        Type = "Weapon",
+        Chance = 6,
+        Crates = {"Fire"},
+    },
+    ["Fire Rocket Launcher"] = {
+        Type = "Weapon",
+        Chance = 1,
+        Crates = {"Fire"},
+    },
+    ["Fire Shotgun"] = {
+        Type = "Weapon",
+        Chance = 11,
+        Crates = {"Fire"},
+    },
+    ["Water Auto Pistol"] = {
+        Type = "Weapon",
+        Chance = 15,
+        Crates = {"Water"},
+    },
+    ["Water Knife"] = {
+        Type = "Weapon",
+        Chance = 22,
+        Crates = {"Water"},
+    },
+    ["Water Pistol"] = {
+        Type = "Weapon",
+        Chance = 19,
+        Crates = {"Water"},
+    },
+    ["Water Rifle"] = {
+        Type = "Weapon",
+        Chance = 6,
+        Crates = {"Water"},
+    },
+    ["Water Rocket Launcher"] = {
+        Type = "Weapon",
+        Chance = 1,
+        Crates = {"Water"},
     },
 }
 
-function Rewards.ChooseRandomReward()
-    return Rewards.AllRewards[math.random(#Rewards.AllRewards)]
+function Rewards.ChooseRandomReward(Crate)
+    return Rewards.Rewards[Crate.Name][math.random(#Rewards.Rewards[Crate.Name])]
+end
+
+function Rewards.ChooseRandomRewards(Crate, Amount)
+    local PossibleRewards = {}
+    for RewardNum = 1, Amount do
+        table.insert(PossibleRewards, Rewards.ChooseRandomReward(Crate))
+    end
+    return PossibleRewards
 end
 
 function Rewards.RunFunction(p, Name)
@@ -45,9 +204,22 @@ function Rewards.Functions.Money(p, Name)
     p.leaderstats.Money.Value = p.leaderstats.Money.Value + Amount
 end
 
+function Rewards.Functions.Weapon(p, Name)
+    local Backpack = p:WaitForChild("Backpack")
+    WeaponStorage[Name]:Clone().Parent = Backpack
+end
+
+-- Creates tables for all of the rewards and crates
+
+for Crate,_ in pairs(Rewards.Crates) do
+    Rewards.Rewards[Crate] = {}
+end
+
 for Name, Info in pairs(Rewards.Objects) do
-    for Chance = 1, Info.Chance do
-        table.insert(Rewards.AllRewards, Name)
+    for _,Crate in pairs(Info.Crates) do
+        for Chance = 1, Info.Chance do
+            table.insert(Rewards.Rewards[Crate], Name)
+        end
     end
 end
 
--- Main/src/ServerScriptService/Crates/init.server.lua
+++ Main/src/ServerScriptService/Crates/init.server.lua
@@ -1,7 +1,7 @@
 -- // Settings \\ --
 
-local DROP_DELAY = 60
-local DROP_COUNT = 4
+local DROP_DELAY = 10
+local DROP_COUNT = 1
 local SPAWN_DELAY = 1
 
 local ORIGINAL_POS = Vector3.new(0, 50, 0)
@@ -18,7 +18,6 @@ local MAX_POSSIBLE_REWARDS = 30
 local Debris = game:GetService("Debris")
 local Rewards = require(script.Rewards)
 local Remotes = game.ReplicatedStorage.Remotes.LootBox
-local Crate = game.ServerStorage.Objects.Crate
 
 -- // Functions \\ --
 
@@ -26,34 +25,31 @@ function Touched(NewCrate, Obj)
     local p = game.Players:FindFirstChild(Obj.Parent.Name)
     if p then
         NewCrate:Destroy()
-        local Reward = Rewards.ChooseRandomReward()
-        
-        local PossibleRewards = {}
-        for RewardNum = 1, MAX_POSSIBLE_REWARDS do
-            table.insert(PossibleRewards, Rewards.ChooseRandomReward())
-        end
-
+        local Reward = Rewards.ChooseRandomReward(NewCrate)        
+        local PossibleRewards = Rewards.ChooseRandomRewards(NewCrate, MAX_POSSIBLE_REWARDS)
         Remotes.Run:FireClient(p, Reward, PossibleRewards)
     end
 end
 
 function CloneCrates()
-    for x = 1, DROP_COUNT do
-        local X = math.random(-X_OFFSET/2, X_OFFSET/2)
-        local Y = math.random(-Y_OFFSET/2, Y_OFFSET/2)
-        local Z = math.random(-Z_OFFSET/2, Z_OFFSET/2)
-
-        local NewCrate = Crate:Clone()
-        NewCrate.Parent = workspace.Debris
-        NewCrate.Position = ORIGINAL_POS + Vector3.new(X, Y, Z)
-        NewCrate.Rotation = Vector3.new(math.random(360), math.random(360), math.random(360))
-
-        NewCrate.Touched:Connect(function(Obj)
-            Touched(NewCrate, Obj)
-        end)
-
-        Debris:AddItem(NewCrate, math.random(MIN_DESPAWN_DELAY, MAX_DESPAWN_DELAY))
-        wait(SPAWN_DELAY)
+    for Name, Crate in pairs(Rewards.Crates) do
+        for x = 1, DROP_COUNT do
+            local X = math.random(-X_OFFSET/2, X_OFFSET/2)
+            local Y = math.random(-Y_OFFSET/2, Y_OFFSET/2)
+            local Z = math.random(-Z_OFFSET/2, Z_OFFSET/2)
+    
+            local NewCrate = Crate:Clone()
+            NewCrate.Parent = workspace.Debris
+            NewCrate.Position = ORIGINAL_POS + Vector3.new(X, Y, Z)
+            NewCrate.Rotation = Vector3.new(math.random(360), math.random(360), math.random(360))
+    
+            NewCrate.Touched:Connect(function(Obj)
+                Touched(NewCrate, Obj)
+            end)
+    
+            Debris:AddItem(NewCrate, math.random(MIN_DESPAWN_DELAY, MAX_DESPAWN_DELAY))
+            wait(SPAWN_DELAY)
+        end
     end
 end
 
